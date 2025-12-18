890ae88618eaa4eb69919e2f5aa7be5854b352a0
luberrima <lucia.beatriz.franco@gmail.com>
2025-12-18 19:00:29 +0100

    dia 39

--- VSCODE/JavaScript/dia_35/js/main.js
+++ VSCODE/JavaScript/dia_35/js/main.js
@@ -1,1 +1,44 @@
-"use strict"
+"use strict";
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+///
+
+
+const sliderWindow = document.querySelector(".slider-window");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+function getScrollAmount() {
+    return sliderWindow.offsetWidth;
+}
+
+nextBtn.addEventListener("click", () => {
+    sliderWindow.scrollBy({
+        left: getScrollAmount(),
+        behavior: "smooth"
+    });
+});
+
+prevBtn.addEventListener("click", () => {
+    sliderWindow.scrollBy({
+        left: -getScrollAmount(),
+        behavior: "smooth"
+    });
+});
